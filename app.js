46a3ef298b5d656206390e9f9ba91df29ab9eae7
Arya Kumar <ar.kr.031@gmail.com>
2023-03-01 17:11:50 +0530

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Banana API!');
+});
+app.get('/banana', function (req, res) {
+  res.send('Banana API! path /banana');
+});
+app.listen(6012, function () {
+  console.log('Example app listening on port 6012!');
+});
