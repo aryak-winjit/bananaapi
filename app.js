791d0a9e1f88f090a5589c11f2ebaa13b56b1c44
Arya Kumar <ar.kr.031@gmail.com>
2023-03-06 17:09:26 +0530

    ud

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('Banana API!');
 });
 app.get('/banana', function (req, res) {
-  res.send('Banana API! path /banana argoCD ' + process.env.VERSION + " " + process.env.DB_PASS + " " + process.env.DB_USER);
+  res.send('/banana ' + process.env.VERSION + " " + process.env.DB_USER + " " + process.env.DB_PASS);
 });
 app.listen(6012, function () {
   console.log('Example app listening on port 6012!' + env_data_owner);
